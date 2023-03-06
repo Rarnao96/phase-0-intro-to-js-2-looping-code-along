82219b1e630b22efb2d1dc71127824acb2bdb5a1
Rachel Arnao <rachel.e.arnao@gmail.com>
2023-03-06 13:42:37 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, value) {
+    const messageNames = [];
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+function countDown(){
+    let favoriteNumber= 10;
+
+    while (favoriteNumber >= 0) {
+        console.log(favoriteNumber);
+        favoriteNumber = favoriteNumber -1;
+    }
+}
